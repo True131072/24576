539bcb7a16b8a5dcb47059b16013535304753c0d
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-10-17 10:46:21 -0500

    Create 24576 variant

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 3 : 6;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -139,8 +139,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 65536) self.won = true;
+          // The mighty 24576 tile
+          if (merged.value === 24576) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
